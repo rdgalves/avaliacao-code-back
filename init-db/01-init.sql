f7017be5d1f396fa08cd1680e0bb354ea6aabf74
Rodrigo Alves da Silva <rodrigos@postalis.com.br>
2023-11-06 08:36:52 -0300

    Ajuste de script inicial, insert users

--- init-db/01-init.sql
+++ init-db/01-init.sql
@@ -10,6 +10,27 @@ CREATE TABLE pessoa (
     CONSTRAINT pk_pessoa PRIMARY KEY (id)
 );
 
+INSERT INTO public.pessoa (nome, datanascimento, cpf, funcionario) VALUES ('João Silva', '1985-04-12 00:00:00', '12345678901', true);
+INSERT INTO public.pessoa (nome, datanascimento, cpf, funcionario) VALUES ('Maria Oliveira', '1990-07-19 00:00:00', '23456789012', false);
+INSERT INTO public.pessoa (nome, datanascimento, cpf, funcionario) VALUES ('Pedro Alcântara', '1975-09-23 00:00:00', '34567890123', true);
+INSERT INTO public.pessoa (nome, datanascimento, cpf, funcionario) VALUES ('Ana Sousa', '2000-01-02 00:00:00', '45678901234', false);
+INSERT INTO public.pessoa (nome, datanascimento, cpf, funcionario) VALUES ('Luiz Costa', '1988-05-15 00:00:00', '56789012345', true);
+INSERT INTO public.pessoa (nome, datanascimento, cpf, funcionario) VALUES ('Patrícia Nobre', '1995-03-28 00:00:00', '67890123456', true);
+INSERT INTO public.pessoa (nome, datanascimento, cpf, funcionario) VALUES ('Rafael Santos', '1982-10-31 00:00:00', '78901234567', false);
+INSERT INTO public.pessoa (nome, datanascimento, cpf, funcionario) VALUES ('Fernanda Gomes', '1999-12-10 00:00:00', '89012345678', true);
+INSERT INTO public.pessoa (nome, datanascimento, cpf, funcionario) VALUES ('Carlos Magno', '1970-06-22 00:00:00', '90123456789', false);
+INSERT INTO public.pessoa (nome, datanascimento, cpf, funcionario) VALUES ('Tatiana Azambuja', '2002-08-05 00:00:00', '01234567890', true);
+INSERT INTO public.pessoa (nome, datanascimento, cpf, funcionario) VALUES ('Roberto Rocha', '1994-04-17 00:00:00', '12345067801', false);
+INSERT INTO public.pessoa (nome, datanascimento, cpf, funcionario) VALUES ('Juliana Queiroz', '1986-11-30 00:00:00', '23456078912', true);
+INSERT INTO public.pessoa (nome, datanascimento, cpf, funcionario) VALUES ('André Lima', '1992-02-20 00:00:00', '34567089023', false);
+INSERT INTO public.pessoa (nome, datanascimento, cpf, funcionario) VALUES ('Camila Porto', '1983-05-25 00:00:00', '45678090134', true);
+INSERT INTO public.pessoa (nome, datanascimento, cpf, funcionario) VALUES ('Marcos Braga', '2001-07-07 00:00:00', '56789001245', false);
+INSERT INTO public.pessoa (nome, datanascimento, cpf, funcionario) VALUES ('Sofia Castelo', '1997-09-09 00:00:00', '67890112356', true);
+INSERT INTO public.pessoa (nome, datanascimento, cpf, funcionario) VALUES ('Ricardo Neves', '1968-12-15 00:00:00', '78901223467', false);
+INSERT INTO public.pessoa (nome, datanascimento, cpf, funcionario) VALUES ('Vanessa Dias', '1993-03-03 00:00:00', '89012334578', true);
+INSERT INTO public.pessoa (nome, datanascimento, cpf, funcionario) VALUES ('Thiago Moraes', '1987-01-01 00:00:00', '90123445689', false);
+
+
 -- -----------------------------------------------------
 -- Table: Projeto
 -- -----------------------------------------------------
